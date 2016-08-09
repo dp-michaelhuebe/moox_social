6ab8482d69883a22439241db7bdfd583ceef1a58
Michael Hübe <michael.huebe@digitalpatrioten.com>
2016-08-09 15:52:35 +0200

    Decreased limit of request posts to 99

--- Classes/Controller/FacebookController.php
+++ Classes/Controller/FacebookController.php
@@ -5,7 +5,7 @@ namespace DCNGmbH\MooxSocial\Controller;
  *  Copyright notice
  *
  *  (c) 2014 Dominic Martin <dm@dcn.de>, DCN GmbH
- *  
+ *
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -32,8 +32,8 @@ use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
  /**
  * Include Facebook API Tools
  */
-require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('moox_social','Classes/Facebook/facebook.php'); 
- 
+require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('moox_social','Classes/Facebook/facebook.php');
+
 /**
  *
  *
@@ -58,29 +58,29 @@ class FacebookController extends \DCNGmbH\MooxSocial\Controller\PostController {
 	 * @inject
 	 */
 	protected $facebookRepository;
-	
+
 	/**
 	 * action index
 	 *
 	 * @return void
 	 */
 	public function indexAction() {
-		
+
 		$query = array(
 			'SELECT' => '*',
 			'FROM' => 'tx_scheduler_task',
 			'WHERE' => '1=1'
 		);
-		
+
 		$res 	= $GLOBALS['TYPO3_DB']->exec_SELECT_queryArray($query);
 		$tasks 	= array();
-		
+
 		foreach($res AS $task){
-			
+
 			$facebooktask = unserialize($task['serialized_task_object']);
-			
+
 			if($facebooktask instanceof \DCNGmbH\MooxSocial\Tasks\FacebookGetTask){
-				$addTask = array();				
+				$addTask = array();
 				$addTask['pid'] 	= $facebooktask->getPid();
 				$addTask['appId'] 	= $facebooktask->getAppId();
 				$addTask['secret'] 	= $facebooktask->getSecret();
@@ -90,9 +90,9 @@ class FacebookController extends \DCNGmbH\MooxSocial\Controller\PostController {
 			}
 		}
 
-		$this->view->assign('tasks', $tasks);				
+		$this->view->assign('tasks', $tasks);
 	}
-	
+
 	/**
 	 * action reinit
 	 *
@@ -102,47 +102,47 @@ class FacebookController extends \DCNGmbH\MooxSocial\Controller\PostController {
 	 * @param string $secret
 	 * @return void
 	 */
-	public function reinitAction($pageId,$storagePid,$appId,$secret) {	
+	public function reinitAction($pageId,$storagePid,$appId,$secret) {
 		if($pageId!=""){
-			
+
 			$this->facebookRepository->removeByPageId($pageId,$storagePid);
-			
-			$rawFeed 	= self::facebook($appId,$secret,$pageId,'init');			
-			
-			$posts 		= array();			
+
+			$rawFeed 	= self::facebook($appId,$secret,$pageId,'init');
+
+			$posts 		= array();
 			$postIds 	= array();
-			
+
 			foreach($rawFeed['data'] as $item) {
-				
+
 				if(!in_array($item['id'],$postIds) && $item['status_type']!=""){
-					
-					$postIds[] 		= $item['id'];					
+
+					$postIds[] 		= $item['id'];
 					$postId 		= explode("_",$item['id']);
 					$postId 		= $postId[1];
-					
+
 					$item['postId'] = $postId;
 					$item['pageId'] = $pageId;
 					$item['pid'] 	= $storagePid;
-					
-					$post 			= self::facebookPost($item);					
-					
+
+					$post 			= self::facebookPost($item);
+
 					if(is_array($post)){
 						$posts[] 	= $post;
 					}
 				}
-				
-			}			
+
+			}
 
 			if(count($posts)){
 
 				$insertCnt = 0;
-				
-				foreach($posts AS $post){				
-										
+
+				foreach($posts AS $post){
+
 					$facebookPost = new \DCNGmbH\MooxSocial\Domain\Model\Facebook;
-					
+
 					$facebookPost->setPid($post['pid']);
-					$facebookPost->setCreated($post['created']);					
+					$facebookPost->setCreated($post['created']);
 					$facebookPost->setUpdated($post['updated']);
 					$facebookPost->setModel("facebook");
 					$facebookPost->setType($post['type']);
@@ -166,22 +166,22 @@ class FacebookController extends \DCNGmbH\MooxSocial\Controller\PostController {
 					$facebookPost->setSharedUrl($post['sharedUrl']);
 					$facebookPost->setSharedTitle($post['sharedTitle']);
 					$facebookPost->setSharedDescription($post['sharedDescription']);
-					$facebookPost->setSharedCaption($post['sharedCaption']);				
+					$facebookPost->setSharedCaption($post['sharedCaption']);
 					$facebookPost->setLikes($post['likes']);
 					$facebookPost->setShares($post['shares']);
 					$facebookPost->setComments($post['comments']);
-					$facebookPost->setApiUid($post['apiUid']);					
+					$facebookPost->setApiUid($post['apiUid']);
 					$facebookPost->setApiHash($post['apiHash']);
-					
+
 					$this->facebookRepository->add($facebookPost);
-					
+
 					$insertCnt++;
-					
-				}	
-				
+
+				}
+
 				$this->objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
 				$this->objectManager->get('TYPO3\CMS\Extbase\Persistence\PersistenceManagerInterface')->persistAll();
-				
+
 				if($insertCnt>0){
 					\DCNGmbH\MooxSocial\Controller\AdministrationController::clearCache("mooxsocial_pi1",array());
 				}
@@ -201,7 +201,7 @@ class FacebookController extends \DCNGmbH\MooxSocial\Controller\PostController {
 		);
 		$this->redirect('index');
 	}
-	
+
 	/**
 	 * action truncate
 	 *
@@ -220,52 +220,52 @@ class FacebookController extends \DCNGmbH\MooxSocial\Controller\PostController {
 		);
 		$this->redirect('index');
 	}
-	
+
 	/**
 	 * action list
 	 *
 	 * @return void
 	 */
 	public function listAction() {
-		
+
 		if($this->settings['use_ajax']){
 			$this->settings['limit'] = $this->settings['ajax_limit'];
 		}
-		
+
 		if(!$this->settings['sort_by']){
 			$this->settings['sort_by'] = "updated";
 		}
-		
+
 		if(!$this->settings['sort_direction']){
 			$this->settings['sort_direction'] = "DESC";
 		}
-		
+
 		if(!$this->settings['limit']){
 			$this->settings['limit'] = 25;
 		}
-		
+
 		if($this->settings['source']=="api"){
-			
+
 			// Get the extensions's configuration
-			$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['moox_social']);		
+			$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['moox_social']);
 			if($this->settings['api_app_id']==""){
 				$this->settings['api_app_id'] = $extConf['fallbackFacebookAppId'];
-			}		
+			}
 			if($this->settings['api_secret']==""){
 				$this->settings['api_secret'] = $extConf['fallbackFacebookSecret'];
 			}
-			
-			if($this->settings['api_app_id']!="" && $this->settings['api_secret']!="" && $this->settings['api_page_id']!=""){				
-				$posts = $this->facebookRepository->requestAllBySettings($this->settings);				
+
+			if($this->settings['api_app_id']!="" && $this->settings['api_secret']!="" && $this->settings['api_page_id']!=""){
+				$posts = $this->facebookRepository->requestAllBySettings($this->settings);
 			}
-			
+
 		} else {
-		
+
 			$count 	= $this->facebookRepository->findAll($this->settings['page_id'])->count();
 
 			$posts 	= $this->facebookRepository->findAllBySettings($this->settings);
 		}
-		
+
 		$this->view->assign('currentpid', $GLOBALS['TSFE']->id);
 		$this->view->assign('posts', $posts);
 		$this->view->assign('count', $count);
@@ -276,64 +276,64 @@ class FacebookController extends \DCNGmbH\MooxSocial\Controller\PostController {
 		$this->view->assign('pages', $pages);
 		$this->view->assign('pagesCount', count($pages));
 		$this->view->assign('maxOffset', (($i-2)*$this->settings['limit']));
-		
+
 	}
-	
+
 	/**
 	 * action listAjax
 	 *
 	 * @return void
 	 */
 	public function listAjaxAction() {
-		
+
 		if(!$this->settings['sort_by']){
 			$this->settings['sort_by'] = "updated";
 		}
-		
+
 		if(!$this->settings['sort_direction']){
 			$this->settings['sort_direction'] = "DESC";
 		}
-		
+
 		if(!$this->settings['limit']){
 			$this->settings['limit'] = 25;
 		}
-		
-		if($this->request->hasArgument('perrequest')){			
-			$this->settings['limit'] = $this->request->getArgument('perrequest');				
-		}
-		
-		if($this->request->hasArgument('offset')){			
-			$this->settings['offset'] = $this->request->getArgument('offset');			
-		}
-		
-		if($this->request->hasArgument('source')){			
-			$this->settings['source'] = $this->request->getArgument('source');				
-		}	
 
-		if($this->request->hasArgument('page')){			
-			$this->settings['api_page_id'] = $this->request->getArgument('page');				
+		if($this->request->hasArgument('perrequest')){
+			$this->settings['limit'] = $this->request->getArgument('perrequest');
 		}
-				
+
+		if($this->request->hasArgument('offset')){
+			$this->settings['offset'] = $this->request->getArgument('offset');
+		}
+
+		if($this->request->hasArgument('source')){
+			$this->settings['source'] = $this->request->getArgument('source');
+		}
+
+		if($this->request->hasArgument('page')){
+			$this->settings['api_page_id'] = $this->request->getArgument('page');
+		}
+
 		if($this->settings['source']=="api"){
-			
+
 			// Get the extensions's configuration
-			$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['moox_social']);		
+			$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['moox_social']);
 			if($this->settings['api_app_id']==""){
 				$this->settings['api_app_id'] = $extConf['fallbackFacebookAppId'];
-			}		
+			}
 			if($this->settings['api_secret']==""){
 				$this->settings['api_secret'] = $extConf['fallbackFacebookSecret'];
-			}			
-			if($this->settings['api_app_id']!="" && $this->settings['api_secret']!="" && $this->settings['api_page_id']!=""){				
-				$posts = $this->facebookRepository->requestAllBySettings($this->settings);								
 			}
-			
+			if($this->settings['api_app_id']!="" && $this->settings['api_secret']!="" && $this->settings['api_page_id']!=""){
+				$posts = $this->facebookRepository->requestAllBySettings($this->settings);
+			}
+
 		} else {
-			$posts 	= $this->facebookRepository->findAllBySettings($this->settings);				
+			$posts 	= $this->facebookRepository->findAllBySettings($this->settings);
 		}
-		
+
 		$this->view->assign('currentpid', $GLOBALS['TSFE']->id);
-		$this->view->assign('posts', $posts);		
+		$this->view->assign('posts', $posts);
 	}
 
 	/**
@@ -343,15 +343,15 @@ class FacebookController extends \DCNGmbH\MooxSocial\Controller\PostController {
 	 * @return void
 	 */
 	public function showAction(\DCNGmbH\MooxSocial\Domain\Model\Facebook $facebook = NULL) {
-		
+
 		if(!$facebook && $this->settings['source']!="api"){
 			$facebook	= $this->facebookRepository->findRandomOne($this->settings['page_id']);
-			$this->view->assign('israndom', TRUE);			
+			$this->view->assign('israndom', TRUE);
 		}
-		
+
 		$this->view->assign('facebook', $facebook);
 	}
-	
+
 	/**
 	 * execute facebook api request
 	 *
@@ -363,25 +363,25 @@ class FacebookController extends \DCNGmbH\MooxSocial\Controller\PostController {
 	 */
 	public function facebook($appId,$secret,$pageId, $request = "") {
 		$rawFeed = array();
-		
+
 		// Get the extensions's configuration
-		$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['moox_social']);		
+		$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['moox_social']);
 		if($appId==""){
 			$appId = $extConf['fallbackFacebookAppId'];
-		}		
+		}
 		if($secret==""){
 			$secret = $extConf['fallbackFacebookSecret'];
 		}
 
 		if($appId!="" && $secret!="" && $pageId!=""){
-						
+
 			$config = array(
 				'appId'				 	=> $appId,
 				'secret'				=> $secret,
 				'pageid' 				=> $pageId,
 				'allowSignedRequest' 	=> false
 			);
-			
+
 			$facebook = new \DCNGmbH\MooxSocial\Facebook\Facebook($config);
 			if($request=="fullinit"){
 				$repeats = 999;
@@ -392,10 +392,10 @@ class FacebookController extends \DCNGmbH\MooxSocial\Controller\PostController {
 			if($request=="init"){
 				$rawFeedData 	= array();
 				$rawFeedIds 	= array();
-				$until 			= time();				
+				$until 			= time();
 				for($i=1;$i<=$repeats;$i++){
 					$since = $until-(604800*16);
-					$request="posts?limit=250&since=".$since."&until=".$until; //."&limit=10000"; limit > 250 is not allowed
+					$request="posts?limit=99&since=".$since."&until=".$until; //."&limit=10000"; limit > 99 is not allowed
 					$url = '/' . $pageId . '/'.$request;
 					$rawFeedTmp = $facebook->api($url);
 					if(count($rawFeedTmp['data'])>0){
@@ -415,24 +415,24 @@ class FacebookController extends \DCNGmbH\MooxSocial\Controller\PostController {
 				$request="posts?limit=25";
 				$url = '/' . $pageId . '/'.$request;
 				$rawFeed = $facebook->api($url);
-			}			
+			}
 		}
-		
+
 		return $rawFeed;
 	}
 
 	/**
 	 * prepare facebook post
 	 *
-	 * @param array $item	
+	 * @param array $item
 	 * @return array post
 	 */
 	public function facebookPost($item) {
-		
+
 		if(is_array($item)){
-			
+
 			$post = array();
-			
+
 			if(strpos($item['status_type'],"shared_")!== false){
 				$post['title'] 				= ""; 						// no match
 				$post['description'] 		= "";
@@ -452,7 +452,7 @@ class FacebookController extends \DCNGmbH\MooxSocial\Controller\PostController {
 			}
 			if($item['type']=="photo" && $item['object_id']>0){
 				$item['picture'] = "https://graph.facebook.com/".$item['object_id']."/picture?type=normal";
-			} elseif(strpos($item['picture'], "safe_image.php")!==false){	
+			} elseif(strpos($item['picture'], "safe_image.php")!==false){
 				$safeImageUrl = urldecode($item['picture']);
 				$safeImageUrl = explode("&url=",$safeImageUrl);
 				$safeImageUrl = $safeImageUrl[1];
@@ -463,7 +463,7 @@ class FacebookController extends \DCNGmbH\MooxSocial\Controller\PostController {
 					$item['picture'] = utf8_encode($safeImageUrl);
 				} else {
 					$item['picture'] = "";
-				}				
+				}
 			}
 			$post['pid'] 					= $item['pid'];
 			$post['created'] 				= strtotime($item['created_time']);
@@ -471,7 +471,7 @@ class FacebookController extends \DCNGmbH\MooxSocial\Controller\PostController {
 			$post['type'] 					= $item['type'];
 			$post['statusType'] 			= $item['status_type'];
 			$post['page'] 					= $item['pageId'];
-			$post['action'] 				= $item['story'];				
+			$post['action'] 				= $item['story'];
 			$post['summary'] 				= $item['summary']; 			// no match
 			$post['text'] 					= $item['message'];
 			$post['author'] 				= $item['from']['name'];
@@ -482,17 +482,17 @@ class FacebookController extends \DCNGmbH\MooxSocial\Controller\PostController {
 			$post['imageUrl'] 				= $item['picture'];
 			$post['imageEmbedcode'] 		= $item['imageEmbedcode']; 		// no match
 			$post['videoUrl'] 				= $item['source'];
-			$post['videoEmbedcode'] 		= $item['videoEmbedcode']; 		// no match				
+			$post['videoEmbedcode'] 		= $item['videoEmbedcode']; 		// no match
 			$post['likes'] 					= count($item['likes']['data']);
 			$post['shares'] 				= count($item['shares']['data']);
 			$post['comments'] 				= count($item['comments']['data']);
-			$post['apiUid'] 				= $item['id'];				
+			$post['apiUid'] 				= $item['id'];
 			$post['apiHash'] 				= md5(print_r($post,TRUE));
-						
+
 			return $post;
-			
+
 		} else {
-		
+
 			return false;
 		}
 	}
